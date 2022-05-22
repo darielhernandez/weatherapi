5bc9090d14f84e5f794d8db3c16ff0e5a408223e
darielhernandez <13dariel@gmail.com>
2022-05-22 13:57:34 -0400

    Weather API completed and working.

--- src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
+++ src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
@@ -1,14 +1,15 @@
 package com.careerdevs.weatherapi.controllers;
 
 
+import com.careerdevs.weatherapi.models.CurrentWeather;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.origin.SystemEnvironmentOrigin;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/current")
@@ -18,11 +19,62 @@ public class CurrentWeatherController {
     @Autowired
     private Environment env;
 
-    @GetMapping("/test")
-    //response entity lets you have a more fine level of control over things like status, data, and headers in your responses
-    public ResponseEntity<?> testRequest(){
+    private final String BASE_URL= "https://api.openweathermap.org/data/2.5/weather";
 
-        String apiKey = env.getProperty("OW_API_KEY");
-        return ResponseEntity.ok("api key: " + apiKey);
+    @GetMapping("/city/{cityName}")
+    //response entity lets you have a more fine level of control over things like status, data, and headers in your responses
+    public ResponseEntity<?> getCurrentWeatherByCityPV (RestTemplate restTemplate, @PathVariable String cityName){
+
+        try {
+            String apiKey = env.getProperty("OW_API_KEY");
+            String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+            String openWeatherURL = BASE_URL + queryString;
+
+            CurrentWeather openWeatherResponse = restTemplate.getForObject(openWeatherURL, CurrentWeather.class);
+
+            assert openWeatherResponse != null;
+            System.out.println("city: " + openWeatherResponse.getName());
+            System.out.println("Temp: " + openWeatherResponse.getMain().getTemp());
+            System.out.println("Desc: " + openWeatherResponse.getWeather()[0].getDescription());
+
+
+
+            return ResponseEntity.ok(openWeatherResponse);
+        }catch (HttpClientErrorException.NotFound e){
+            return ResponseEntity.status(404).body("City Not Found: " + cityName);
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
     }
+
+    @GetMapping("/")
+    //response entity lets you have a more fine level of control over things like status, data, and headers in your responses
+    public ResponseEntity<?> getCurrentWeatherByCityRP (RestTemplate restTemplate, @RequestParam("city") String cityName){
+
+        try {
+            String apiKey = env.getProperty("OW_API_KEY");
+            String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+            String openWeatherURL = BASE_URL + queryString;
+
+            String openWeatherResponse = restTemplate.getForObject(openWeatherURL, String.class);
+
+            return ResponseEntity.ok(openWeatherResponse);
+
+        }catch (HttpClientErrorException.NotFound e){
+            return ResponseEntity.status(404).body("City not found: " + cityName);
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+    }
+
 }
--- src/main/java/com/careerdevs/weatherapi/models/CurrentWeather.java
+++ src/main/java/com/careerdevs/weatherapi/models/CurrentWeather.java
@@ -1,4 +1,111 @@
 package com.careerdevs.weatherapi.models;
 
+
 public class CurrentWeather {
+
+    private String name;
+
+    private int timezone;
+
+    private int visibility;
+
+    private Coords coord;
+
+    private Main main;
+
+    private Weather[] weather;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTimezone() {
+        return timezone;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public Coords getCoord() {
+        return coord;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public Weather[] getWeather() {
+        return weather;
+    }
+
+    public static class Coords {
+        private float lon;
+        private float lat;
+
+        public float getLon() {
+            return lon;
+        }
+
+        public float getLat() {
+            return lat;
+        }
+    }
+
+    public static class Main{
+        private float temp;
+        private float feels_like;
+        private float temp_min;
+        private float temp_max;
+        private int pressure;
+        private float humidity;
+
+        public float getTemp() {
+            return temp;
+        }
+
+        public float getFeels_like() {
+            return feels_like;
+        }
+
+        public float getTemp_min() {
+            return temp_min;
+        }
+
+        public float getTemp_max() {
+            return temp_max;
+        }
+
+        public int getPressure() {
+            return pressure;
+        }
+
+        public float getHumidity() {
+            return humidity;
+        }
+    }
+
+    public static class Weather{
+
+        private int id;
+        private String main;
+        private String description;
+        private String icon;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+    }
 }
