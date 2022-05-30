84915bba834fdee8d6584c5bc6c7c6d01b7b34aa
darielhernandez <13dariel@gmail.com>
2022-05-30 10:28:39 -0400

    Weather API 2 completed and working.

--- src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
+++ src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
@@ -2,6 +2,7 @@ package com.careerdevs.weatherapi.controllers;
 
 
 import com.careerdevs.weatherapi.models.CurrentWeather;
+import com.careerdevs.weatherapi.models.CurrentWeatherReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.origin.SystemEnvironmentOrigin;
 import org.springframework.core.env.Environment;
@@ -15,7 +16,6 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api/current")
 public class CurrentWeatherController {
 
-
     @Autowired
     private Environment env;
 
@@ -26,20 +26,31 @@ public class CurrentWeatherController {
     public ResponseEntity<?> getCurrentWeatherByCityPV (RestTemplate restTemplate, @PathVariable String cityName){
 
         try {
+            String units= "imperial";
             String apiKey = env.getProperty("OW_API_KEY");
             String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
             String openWeatherURL = BASE_URL + queryString;
 
-            CurrentWeather openWeatherResponse = restTemplate.getForObject(openWeatherURL, CurrentWeather.class);
+            CurrentWeather owRes = restTemplate.getForObject(openWeatherURL, CurrentWeather.class);
 
-            assert openWeatherResponse != null;
-            System.out.println("city: " + openWeatherResponse.getName());
-            System.out.println("Temp: " + openWeatherResponse.getMain().getTemp());
-            System.out.println("Desc: " + openWeatherResponse.getWeather()[0].getDescription());
+            assert owRes != null;
+//            System.out.println("city: " + openWeatherResponse.getName());
+//            System.out.println("Temp: " + openWeatherResponse.getMain().getTemp());
+//            System.out.println("Desc: " + openWeatherResponse.getWeather()[0].getDescription());
+            CurrentWeatherReport report = new CurrentWeatherReport(
+                    owRes.getName(),
+                    owRes.getCoord(),
+                    owRes.getMain(),
+                    owRes.getWeather()[0],
+                    units
+
+            );
+
+            System.out.println(report);
 
 
+            return ResponseEntity.ok(report);
 
-            return ResponseEntity.ok(openWeatherResponse);
         }catch (HttpClientErrorException.NotFound e){
             return ResponseEntity.status(404).body("City Not Found: " + cityName);
 
@@ -52,29 +63,29 @@ public class CurrentWeatherController {
 
     }
 
-    @GetMapping("/")
-    //response entity lets you have a more fine level of control over things like status, data, and headers in your responses
-    public ResponseEntity<?> getCurrentWeatherByCityRP (RestTemplate restTemplate, @RequestParam("city") String cityName){
-
-        try {
-            String apiKey = env.getProperty("OW_API_KEY");
-            String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
-            String openWeatherURL = BASE_URL + queryString;
-
-            String openWeatherResponse = restTemplate.getForObject(openWeatherURL, String.class);
-
-            return ResponseEntity.ok(openWeatherResponse);
-
-        }catch (HttpClientErrorException.NotFound e){
-            return ResponseEntity.status(404).body("City not found: " + cityName);
-
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-            System.out.println(e.getClass());
-
-            return ResponseEntity.internalServerError().body(e.getMessage());
-        }
-
-    }
+//    @GetMapping("/")
+//    //response entity lets you have a more fine level of control over things like status, data, and headers in your responses
+//    public ResponseEntity<?> getCurrentWeatherByCityRP (RestTemplate restTemplate, @RequestParam("city") String cityName){
+//
+//        try {
+//            String apiKey = env.getProperty("OW_API_KEY");
+//            String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+//            String openWeatherURL = BASE_URL + queryString;
+//
+//            String openWeatherResponse = restTemplate.getForObject(openWeatherURL, String.class);
+//
+//            return ResponseEntity.ok(openWeatherResponse);
+//
+//        }catch (HttpClientErrorException.NotFound e){
+//            return ResponseEntity.status(404).body("City not found: " + cityName);
+//
+//        } catch (Exception e){
+//            System.out.println(e.getMessage());
+//            System.out.println(e.getClass());
+//
+//            return ResponseEntity.internalServerError().body(e.getMessage());
+//        }
+//
+//    }
 
 }
---
+++ src/main/java/com/careerdevs/weatherapi/models/CurrentWeatherReport.java
@@ -0,0 +1,126 @@
+package com.careerdevs.weatherapi.models;
+
+public class CurrentWeatherReport {
+
+    private String name;
+    private String main;
+    private String description;
+    private String units;
+    private float temp;
+    private float feelsLike;
+    private float tempMin;
+    private float tempMax;
+    private float pressure;
+    private float humidity;
+    private float lat;
+    private float lon;
+
+    public CurrentWeatherReport(
+            String name,
+            CurrentWeather.Coords coords,
+            CurrentWeather.Main main,
+            CurrentWeather.Weather weather,
+            String units
+    ) {
+        this.units = units;
+        this.name = name;
+        lon = coords.getLon();
+        lat = coords.getLat();
+        temp = main.getTemp();
+        feelsLike = main.getFeels_like();
+        tempMin = main.getTemp_min();
+        tempMax = main.getTemp_max();
+        pressure = main.getPressure();
+        humidity = main.getHumidity();
+        this.main = weather.getMain();
+        description = weather.getDescription();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public float getTemp() {
+        return temp;
+    }
+
+    public float getFeelsLike() {
+        return feelsLike;
+    }
+
+    public float getTempMin() {
+        return tempMin;
+    }
+
+    public float getTempMax() {
+        return tempMax;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "CurrentWeatherReport{" +
+//                "units='" + units + '\'' +
+//                ", name='" + name + '\'' +
+//                ", lon=" + lon +
+//                ", lat=" + lat +
+//                ", temp=" + temp +
+//                ", feelsLike=" + feelsLike +
+//                ", tempMin=" + tempMin +
+//                ", tempMax=" + tempMax +
+//                ", pressure=" + pressure +
+//                ", humidity=" + humidity +
+//                ", main='" + main + '\'' +
+//                ", description='" + description + '\'' +
+//                '}';
+//    }
+
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("CurrentWeatherReport{");
+        sb.append("\"name\":\"").append(name).append('"');
+        sb.append(", \"main\":\"").append(main).append('"');
+        sb.append(", \"description\":\"").append(description).append('"');
+        sb.append(", \"units\":\"").append(units).append('"');
+        sb.append(", \"temp\":").append(temp).append(units.equals("imperial") ? "F" : "C").append("°\"");
+        sb.append(", \"feelsLike\":").append(feelsLike).append(units.equals("imperial") ? "F" : "C").append("°\"");
+        sb.append(", \"tempMin\":").append(tempMin).append(units.equals("imperial") ? "F" : "C").append("°\"");
+        sb.append(", \"tempMax\":").append(tempMax).append(units.equals("imperial") ? "F" : "C").append("°\"");
+        sb.append(", \"pressure\":").append(pressure);
+        sb.append(", \"humidity\":").append(humidity);
+        sb.append(", \"lat\":").append(lat);
+        sb.append(", \"lon\":").append(lon);
+        sb.append('}');
+        return sb.toString();
+    }
+}
+
